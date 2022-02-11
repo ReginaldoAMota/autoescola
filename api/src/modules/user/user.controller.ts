edfc1d4d60094fbbec0421b1340717f92f996459
Reginaldo Adriano Mota <reginaldo_adriano@live.com>
2022-02-11 16:03:21 -0300

    .

---
+++ api/src/modules/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getHello(): string {
+    return this.userService.getUsers();
+  }
+  @Get(':id')
+  get(): string {
+    return 'rolou de mais';
+  }
+}
